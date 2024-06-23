0323f511f2598cc406ef55c5f10bcfdb1f04a2d4
MariaLvova <lvovamaria0202@gmail.com>
2024-06-23 02:57:04 +0200

    Štruktúra MAT a Makro ELEM

--- mat_multiply/main.c
+++ mat_multiply/main.c
@@ -1,1 +1,13 @@
-﻿
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Definícia štruktúry MAT
+typedef struct {
+    unsigned int rows;
+    unsigned int cols;
+    int* elem; // Pole pre prvky matice
+} MAT;
+
+// Definícia makra na prístup k prvkom matice
+#define ELEM(mat, i, j) ((mat)->elem[(i) * (mat)->cols + (j)])
